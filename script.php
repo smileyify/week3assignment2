3a9dfbdf54f73100a6a4c564ea4c6c8156e0eeeb
smileyify <133359694+smileyify@users.noreply.github.com>
2023-05-25 11:18:25 +0100

    Create script.php

---
+++ script.php
@@ -0,0 +1,14 @@
+<?php
+
+$name = "Arua Favour Ifeanyichukwu";
+$regnumber = "2021/HND/34273/CS";
+$soo = "Ebonyi State";
+$topic = "Design and Implementation of Product Expiry Alert Management System";
+
+
+echo "Name: $name<br>";
+echo "Regnumber: $regnumber<br>";
+echo "State of origin: $soo<br>";
+echo "Project topic: $topic<br>";
+
+?>
